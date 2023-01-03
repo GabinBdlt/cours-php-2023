7d5d013aa56688d634cd54631e8aa707e5160bcf
GabinBdlt <gabin.baudrillart35@gmail.com>
2023-01-03 11:54:00 +0100

    Nouveau type de variable : les entiers

--- variables.php
+++ variables.php
@@ -53,3 +53,98 @@ $autrePhrase = ''; // J'initialise, un seul égal =
 $autrePhrase .= $prenom; // Je remplis .=
 $autrePhrase .= ' is in the ';
 $autrePhrase .= $piece;
+
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
+
+// Deuxième type de variables, les nombres ENTIERS
+
+$unNombre = 35;
+$unAutreNombre = 11;
+
+// Il y a une limite à ce que peut contenir un entier
+// PHP n'est pas "très bon" pour gérer des grands nombres.
+
+$UnNombreNegatif = -7;
+
+// Calculs
+// 5 opérateurs importants pour faire des calculs
+// Addition : +
+// Soustraction : - (tiret du 6)
+// Multiplication : * (étoile)
+// Division : / (slash)
+// Modulo : % (reste de la division euclidienne)
+
+$calcul = $unNombre + 9;
+echo $sautDeLigne;
+echo $calcul;
+
+$calcul = $unAutreNombre - 34;
+echo $sautDeLigne;
+echo $calcul;
+
+$calcul = $unNombre * $unAutreNombre;
+echo $sautDeLigne;
+echo $calcul;
+
+$calcul = $unNombre + $unNombre + $unNombre + $unAutreNombre;
+echo $sautDeLigne;
+echo $calcul;
+
+$calcul = $unNombre + ($unAutreNombre * $UnNombreNegatif);
+echo $sautDeLigne;
+echo $calcul;
+
+$positionGrille = 6;
+$penaliteBoite = 5;
+$penalitePiste = 10;
+
+$calcul = $positionGrille + $penaliteBoite + $penalitePiste;
+echo $sautDeLigne;
+echo $calcul;
+
+$nombrePilote = 44;
+$nombreGroupe = 4;
+
+$calcul = $nombrePilote / $nombreGroupe;
+echo $sautDeLigne;
+echo $calcul;
+
+// On peut faire des opérations mathématiques aussi complexe que nécessaires.
+
+// Opération condensés
+// Addition : +=
+// Soustraction : -= 
+// Multiplication : *=
+// Division : /= 
+// Modulo : %=
+
+$somme = 0;
+$somme = $somme + 1; //1
+$somme = $somme + 2; //3
+$somme = $somme + 3; //6
+
+// Version condensée 
+$somme = 0;
+$somme += 1;
+$somme += 2;
+$somme += 3;
+
+// += et -= sont souvent utilisés
+// Les autres beaucoup moins
+// C'est à l'appréciation du développeur
+
+// Opérateur d'incrémentation et décrémentation 
+
+$compteur = 5;
+$compteur++; //6
+
+$autreCompteur = 12;
+$autreCompteur--; //11
+
+$a = 1;
+// Ces 3 lignes font la même chose :
+$a = $a +1;
+$a += 1;
+$a++;
